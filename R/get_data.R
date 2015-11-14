ecc8149b79c6b52e713bbe88121cff3324e27e11
Minsu-Daniel-Kim <minsu.daniel.kim@gmail.com>
2015-11-14 11:27:33 -0800

    first commit

---
+++ R/get_data.R
@@ -0,0 +1,14 @@
+
+#' Carpool retrieve data from server
+#'
+#' This function allows you to retrieve data from server.
+#' @param user id
+#' @keywords carpool
+#' @export
+#' @examples
+#' get_data()
+get_data <- function() {
+  
+  print("get data from server!")
+  
+}
